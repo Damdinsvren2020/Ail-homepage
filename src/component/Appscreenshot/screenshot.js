6ff069c017486be381f2abc2a8eac1337b78d5f8
Damdinsvren2020 <Damdinsvren9548@gmail.com>
2021-08-06 14:26:41 +0800

    App screenshot

--- src/component/Appscreenshot/screenshot.js
+++ src/component/Appscreenshot/screenshot.js
@@ -1,21 +1,26 @@
-import React from 'react';
-// import axios from 'axios';
-
-const Screenshot = () => {
-    // const [image_neg,setImage_neg] = useState([]);
-    // useEffect(() => {
-    //     axios
-    //     .get(`${process.env.REACT_APP_API_URL}api/v1/App_screenshot`)
-    //     .then((res) => {
-    //         setImage_neg(res.data.data);
-    //         console.log(res.data.data);
-    //     })
-    //     .catch((err) => {
-    //         console.log(err);
-    //     })
-    // });
-   return (
-    <section id="ei-screenshots" className="ei-screenshots-section position-relative">
+import React ,{Component} from 'react';
+import axios from 'axios';
+class Screenshot extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            zurguud: []
+        }
+    }
+    componentDidMount() {
+        axios.get('http://103.29.144.253:8092/api/v1/App_screenshot')
+          .then(response => {
+            const zurguud = response.data.data[0];
+            console.log('App screenshot ehnii zurag',response.data.data[0])
+            this.setState({ zurguud: zurguud });
+          })
+          .catch(error => {
+            console.log(error)
+        })
+    } 
+    render(){
+        return (
+            <section id="ei-screenshots" className="ei-screenshots-section position-relative">
     <div className="container">
         <div className="eight-section-title appeight-headline pera-content text-center">
             <h2>Системийн мэдээлэл 
@@ -25,12 +30,86 @@ const Screenshot = () => {
             <div className="row ei-appScreenshotCarousel-container swiper-container">
                 <div className="ei-screen-mobile-image"></div>
                 <div className="swiper-wrapper">
-                    <div className="swiper-slide" style={{backgroundImage:`url(assets/img/app-landing/screenshoot/screenshot01.png)`}}></div>
-                    <div className="swiper-slide" style={{backgroundImage:`url(assets/img/app-landing/screenshoot/screenshot02.png)`}}></div>
-                    <div className="swiper-slide" style={{backgroundImage:`url(assets/img/app-landing/screenshoot/screenshot03.png)`}}></div>
-                    <div className="swiper-slide" style={{backgroundImage:`url(assets/img/app-landing/screenshoot/screenshot04.png)`}}></div>
-                    <div className="swiper-slide" style={{backgroundImage:`url(assets/img/app-landing/screenshoot/search.png)`}}></div>
-                    <div className="swiper-slide" style={{backgroundImage:`url(assets/img/app-landing/screenshoot/search.png)`}}></div>
+                    <div className="swiper-slide" style=
+                        {
+                            this.state.zurguud && this.state.zurguud.image_neg &&
+                            {backgroundImage:`url(${process.env.REACT_APP_API_URL}/storage/app/media${this.state.zurguud.image_neg})`}                       
+
+                        } 
+                        >                       
+                     </div>
+                     <div className="swiper-slide" style=
+                        {
+                            this.state.zurguud && this.state.zurguud.image_neg &&
+                            {backgroundImage:`url(${process.env.REACT_APP_API_URL}/storage/app/media${this.state.zurguud.image_hoyor})`}                       
+
+                        } 
+                        >                       
+                     </div>
+                     <div className="swiper-slide" style=
+                        {
+                            this.state.zurguud && this.state.zurguud.image_neg &&
+                            {backgroundImage:`url(${process.env.REACT_APP_API_URL}/storage/app/media${this.state.zurguud.image_guraw})`}                       
+
+                        } 
+                        >                       
+                     </div>
+                     <div className="swiper-slide" style=
+                        {
+                            this.state.zurguud && this.state.zurguud.image_neg &&
+                            {backgroundImage:`url(${process.env.REACT_APP_API_URL}/storage/app/media${this.state.zurguud.image_duruw})`}                       
+
+                        } 
+                        >                       
+                     </div>
+                     <div className="swiper-slide" style=
+                        {
+                            this.state.zurguud && this.state.zurguud.image_neg &&
+                            {backgroundImage:`url(${process.env.REACT_APP_API_URL}/storage/app/media${this.state.zurguud.image_tav})`}                       
+
+                        } 
+                        >                       
+                     </div>
+                     <div className="swiper-slide" style=
+                        {
+                            this.state.zurguud && this.state.zurguud.image_neg &&
+                            {backgroundImage:`url(${process.env.REACT_APP_API_URL}/storage/app/media${this.state.zurguud.image_zurgaa})`}                       
+
+                        } 
+                        >                       
+                     </div>
+                     <div className="swiper-slide" style=
+                        {
+                            this.state.zurguud && this.state.zurguud.image_neg &&
+                            {backgroundImage:`url(${process.env.REACT_APP_API_URL}/storage/app/media${this.state.zurguud.image_doloo})`}                       
+
+                        } 
+                        >                       
+                     </div>
+                     <div className="swiper-slide" style=
+                        {
+                            this.state.zurguud && this.state.zurguud.image_neg &&
+                            {backgroundImage:`url(${process.env.REACT_APP_API_URL}/storage/app/media${this.state.zurguud.image_naim})`}                       
+
+                        } 
+                        >                       
+                     </div>
+                     <div className="swiper-slide" style=
+                        {
+                            this.state.zurguud && this.state.zurguud.image_neg &&
+                            {backgroundImage:`url(${process.env.REACT_APP_API_URL}/storage/app/media${this.state.zurguud.image_ys})`}                       
+
+                        } 
+                        >                       
+                     </div>
+                     <div className="swiper-slide" style=
+                        {
+                            this.state.zurguud && this.state.zurguud.image_neg &&
+                            {backgroundImage:`url(${process.env.REACT_APP_API_URL}/storage/app/media${this.state.zurguud.image_araw})`}                       
+
+                        } 
+                        >                       
+                     </div>
                 </div>
             </div>
             <div className="banner-navigation">
@@ -41,7 +120,8 @@ const Screenshot = () => {
         <div className="screenshoot-vector screenshoot-shape1 position-absolute"> <img src="assets/img/app-landing/shape/ss-shape1.png" alt=""/></div>
         <div className="screenshoot-vector screenshoot-shape2 position-absolute"> <img src="assets/img/app-landing/shape/ss-shape2.png" alt=""/></div>
     </section>
-   );
+        )
+    }
 }
 
 export default Screenshot;
